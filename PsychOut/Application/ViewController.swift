331e0a51bc7b7e011e366a699c253c3b2e5691eb
Chirag Davé <c@chirag.io>
2019-08-20 17:04:11 -0400

    Add minimum UI to storyboard

--- PsychOut/Application/ViewController.swift
+++ PsychOut/Application/ViewController.swift
@@ -11,12 +11,14 @@ import MetalKit
 
 class ViewController: NSViewController {
   var renderer: Renderer?
+  
+  @IBOutlet weak var metalView: MTKView?
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    guard let metalView = view as? MTKView else {
-      fatalError("metal view not setup in storyboard")
+    guard let metalView = metalView else {
+      fatalError("metalView not setup in storyboard")
     }
     
     renderer = Renderer()
@@ -24,7 +26,6 @@ class ViewController: NSViewController {
     
     // So we can write to textures via compute shader
     metalView.framebufferOnly = false
-    metalView.clearColor = MTLClearColor(red: 1, green: 0, blue: 0, alpha: 1.0)
     metalView.delegate = renderer
     
   }
